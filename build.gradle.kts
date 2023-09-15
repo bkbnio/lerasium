e1a150145725417193fb02315f0024151586b2fd
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2023-09-15 12:52:22 -0400

    chore: bump versions and stuff (#149)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ import io.bkbn.sourdough.gradle.library.jvm.LibraryJvmExtension
 plugins {
   kotlin("jvm") version "1.9.10" apply false
   kotlin("plugin.serialization") version "1.9.10" apply false
-  id("io.bkbn.sourdough.library.jvm") version "0.12.0" apply false
-  id("io.bkbn.sourdough.application.jvm") version "0.12.0" apply false
+  id("io.bkbn.sourdough.library.jvm") version "0.12.1" apply false
+  id("io.bkbn.sourdough.application.jvm") version "0.12.1" apply false
   id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
-  id("io.bkbn.sourdough.root") version "0.12.0"
+  id("io.bkbn.sourdough.root") version "0.12.1"
   id("com.github.jakemarsden.git-hooks") version "0.0.2"
-  id("org.jetbrains.kotlinx.kover") version "0.6.1"
+  id("org.jetbrains.kotlinx.kover") version "0.7.3"
   id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
@@ -20,6 +20,10 @@ gitHooks {
       "pre-push" to "test"
     )
   )
+}
+
+dependencies {
+  subprojects.forEach { kover(it) }
 }
 
 allprojects {
